7ef06e8ebee0b7eb3180f184bc46b622d62d1998
Cristopher Bautista <cristopher.bautista1f@gmail.com>
2025-04-28 10:08:31 -0500

    Quitar nullables

--- Quote/Mapper.cs
+++ Quote/Mapper.cs
@@ -94,29 +94,34 @@ namespace Quote
                 tourQuotes = tourQuotes.Where(w => selectedServicesCodes.Contains(w.ContractService.ServiceCode)).ToList();
             }
 
-            var baseSelectedQuote = tourQuotes.First();
-            var margin = .25;
             TourCalculatedQuote calculatedQuote = null;
-            if (request.RetrieveOptions.GetCalculatedQuote)
+
+            if (tourQuotes.Count() > 0)
             {
-                calculatedQuote = new TourCalculatedQuote()
-                {
-                    baseSelectedQuote = baseSelectedQuote,
-                    allSelectedQuotes = tourQuotes,
-                    maxPax = baseSelectedQuote.maxPax,
-                    Tour = tour,
-                    services = activityServices,
-                    Margin = (double)margin,
-                    EngineVersion = 1
-                };
+                var baseSelectedQuote = tourQuotes.First();
+                var margin = .25;
 
-                foreach (var tourQuote in tourQuotes)
+                if (request.RetrieveOptions.GetCalculatedQuote)
                 {
-                    var modality = activity.Activity.Modalities.FirstOrDefault(f => f.Code == tourQuote.ContractService.ServiceCode);
-                    this.SetTourCalculatedQuote(request, calculatedQuote, tourQuote, modality, exchangeRate);
+                    calculatedQuote = new TourCalculatedQuote()
+                    {
+                        baseSelectedQuote = baseSelectedQuote,
+                        allSelectedQuotes = tourQuotes,
+                        maxPax = baseSelectedQuote.maxPax,
+                        Tour = tour,
+                        services = activityServices,
+                        Margin = (double)margin,
+                        EngineVersion = 1
+                    };
+
+                    foreach (var tourQuote in tourQuotes)
+                    {
+                        var modality = activity.Activity.Modalities.FirstOrDefault(f => f.Code == tourQuote.ContractService.ServiceCode);
+                        this.SetTourCalculatedQuote(request, calculatedQuote, tourQuote, modality, exchangeRate);
+                    }
+
+                    calculatedQuote.CancellationPolicies = this.GetCancellationPolicies(request, calculatedQuote, currency, modalities);
                 }
-
-                calculatedQuote.CancellationPolicies = this.GetCancellationPolicies(request, calculatedQuote, currency, modalities);
             }
 
             var result = new TourQuoteResponse()
